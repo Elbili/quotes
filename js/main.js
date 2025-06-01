a87f616a0eb7eff85eb604d5389b1170face12bd
Elbili <omarshabanfekry@gmail.com>
2025-06-01 15:42:24 +0300

    secound commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@ var Quote = [
     {quotes: "A room without books is like a body without a soul." ,author: "― Marcus Tullius Cicero" },
     {quotes: "In three words I can sum up everything I've learned about life: it goes on." ,author: "― Robert Frost" },
     {quotes: "It is better to be hated for what you are than to be loved for what you are not." ,author: "― Andre Gide" },
+    //
+    {quotes: " Creativity is allowing yourself to make mistakes. Art is knowing which ones to keep." ,author: "― Scott Adams" },
+    {quotes: "ntil you value yourself, you will not value your time. Until you value your time, you will not do anything with it." ,author: "― M. Scott Peck" },
+    {quotes: "Success is the sum of small efforts, repeated day in and day out." ,author: "― Robert Collier" },
+    {quotes: "Don’t wait for inspiration. It comes while working. " ,author: "― Henri Matisse" },
+    {quotes: "راحة الجسم في قلّة الطعام، وراحة النفس في قلّة الآثام، وراحة القلب في قلّة الاهتمام، وراحة اللسان في قلة الكلام" ,author: "― Henri Matisse" },
+    {quotes: "Successful people are not gifted; they just work hard, and then succeed on purpose." ,author: "― G.K. Nielson" },
+    {quotes: "Don’t wait for inspiration. It comes while working." ,author: "― Henri Matisse" },
+    {quotes: "Success isn’t overnight. It’s when every day you get a little better than the day before. It all adds up." ,author: "― علي ابن أبي طالب"},
+    {quotes: "الغرب ليسوا عباقرة ونحن لسنا أغبياء؛ هم فقط يدعمون الفاشل حتى ينجح، ونحن نحارب الناجح حتى يفشل!." ,author: "― أحمد زويل" },
+    {quotes: "المرء دائما حيث يضع نفسه." ,author: "― أحمد زويل" },
 ];
 function getQuotes() {
     var container = Math.floor(Math.random() * Quote.length);
